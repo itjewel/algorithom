e2d181b3b3d5c97f0b0642f7ce0f23310972ac0d
md.jewel <md.jewelmia52@gmail.com>
2025-02-11 17:55:00 +0600

    solved

--- test_dynamic.js
+++ test_dynamic.js
@@ -3,19 +3,19 @@ let arr = [7, 6, 8, 9, 3, 5, 4, 6, 2, 9, 5, 1];
 let target = 11;
 
 /* Need to find target value sum from array give me index */
-let map = [];
+let map = {};
+let res = [];
 let getSumValue = (array, tar) => {
   let total = array.length;
   for (i = 0; i < total; i++) {
-    for (j = i + 1; j < total; j++) {
-      let sum = arr[i] + arr[j];
-      if (sum === tar) {
-        map.push([i, j]);
-      }
+    let complement = tar - arr[i];
+    if (map[complement] !== undefined) {
+      res.push([map[complement], i]);
     }
-    //
+    map[array[i]] = i;
+    //console.log(complement);
   }
-  return map;
+  return res ? res : "not found";
 };
 
 let result = getSumValue(arr, target);
