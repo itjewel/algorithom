29404500ff4349cf962b7d1a01794c959fdfb288
md.jewel <md.jewelmia52@gmail.com>
2025-07-22 17:42:11 +0600

    added prictice

--- goalgorithom/practice.go
+++ goalgorithom/practice.go
@@ -1,10 +1,17 @@
 /* short variable declaration in if statement */
-people := map[string]int{
+package main
+
+import "fmt"
+var people = map[string]int{
     "Alice":   25,
     "Bob":     30,
     "Charlie": 40,
 }
 
+
+func Practice(){
 if age, found := people["charlie"]; found {
 	fmt.Println("got it", age)
+}
+
 }
