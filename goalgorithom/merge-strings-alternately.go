0aa60c821db11eba9371455247b84f1d6a15ae84
md.jewel mia <md.jewelmia52@gmail.com>
2025-07-26 21:58:11 +0600

    merger string

--- goalgorithom/merge-strings-alternately.go
+++ goalgorithom/merge-strings-alternately.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+var str1 = "abc"
+var str2 = "pqr"
+
+func mergerString() {
+	maxNum := max(len(str1), len(str2))
+	result := ""
+	for i := 0; i < maxNum; i++ {
+		if i < len(str1) {
+			result += string(str1[i])
+		}
+		if i < len(str2) {
+			result += string(str2[i])
+		}
+	}
+	fmt.Println("", result)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 func main() {
-	fmt.Println("hello world")
+	mergerString()
 }
