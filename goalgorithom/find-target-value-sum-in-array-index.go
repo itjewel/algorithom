0635f6cbe984f4c200a9d5130467261651574053
md.jewel <md.jewelmia52@gmail.com>
2025-07-26 18:04:01 +0600

    added fix

--- goalgorithom/find-target-value-sum-in-array-index.go
+++ goalgorithom/find-target-value-sum-in-array-index.go
@@ -1,27 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var arr = []int{1, 7, 2, 3, 2, 3, 6}
 var targetNum = 9
-
-func findTargetValue() {
-	//fmt.Println("Array:", arr)
-	// HashMap approach: value -> index
-	numMap := make(map[int]int)
-
+func targetValueIntex(){
+	var makeMap = make(map[int]int)
 	for i, num := range arr {
-		complement := targetNum - num
-		if idx, found := numMap[complement]; found {
-			fmt.Printf("Target %d found at indices: [%d, %d]\n", targetNum, idx, i)
-			return
+		var currentNum = targetNum - num;
+		if index, status := makeMap[currentNum]; status {
+			fmt.Println("Hello",i, index);
+			return;
+		}else{
+			makeMap[num] = i
 		}
-		numMap[num] = i
+		
 	}
 
-	fmt.Println("No two numbers found with the target sum.")
 }
 
 func main() {
-	findTargetValue()
+	targetValueIntex()
 }
