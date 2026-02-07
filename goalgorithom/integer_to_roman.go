2a82643ceea6de1a1721b28e93e9ebfad1192036
md.jewel mia <md.jewelmia52@gmail.com>
2026-02-07 16:33:00 +0600

    added integer to roman

--- goalgorithom/integer_to_roman.go
+++ goalgorithom/integer_to_roman.go
@@ -6,25 +6,37 @@ import "fmt"
 func intToRoman(num int) string {
 	symbleValue := map[int]int{
 	0:1000,
-	1:500,
-	2:100,
-	3:50,
-	4:10,
-	5:5,
-	6:1,
+	1:900,
+	2:500,
+	3:400,
+	4:100,
+	5:90,
+	6:50,
+	7:40,
+	8:10,
+	9:9,
+	10:5,
+	11:4,
+	12:1,
 }
 symbleRoman := map[int]string{
 	0: "M",
-	1: "D",
-	2: "C",
-	3: "L",
-	4: "X",
-	5: "V",
-	6: "I",
+	1: "CM",
+	2: "D",
+	3: "CD",
+	4: "C",
+	5: "XC",
+	6: "L",
+	7: "XL",
+	8: "X",
+	9: "IX",
+	10: "V",
+	11: "IV",
+	12: "I",
 }
     output := ""
 	for i:=0; i<len(symbleValue); i++{
-		if symbleValue[i]>=num{
+		for symbleValue[i] <= num {
 			output += symbleRoman[i]
 			num -= symbleValue[i]
 		}
