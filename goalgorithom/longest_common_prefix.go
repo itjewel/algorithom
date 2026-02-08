24adb67a061150329d0230d7d1ee46034188b617
md.jewel mia <md.jewelmia52@gmail.com>
2026-02-08 23:42:06 +0600

    longest common prefix added

--- goalgorithom/longest_common_prefix.go
+++ goalgorithom/longest_common_prefix.go
@@ -3,8 +3,23 @@ import "fmt"
 
 
 func longestCommonPrefix(strs []string) string {
-    prefix := ""
+    if len(strs) == 0{
+		return ""
+	}
+	prefix := ""
+	
+	for i:=0; i<len(strs[0]); i++{
+		ch := strs[0][i]
+		for j:=1; j<len(strs); j++{
+			if i >=len(strs[j]) || strs[j][i] != ch {
+				return prefix
+			}
+		}
+		prefix += string(ch)
+		
+	}
 
+fmt.Println(prefix)
 	return prefix
 }
 
