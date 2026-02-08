0c064ba621d19baf573bd48772f6a614305bdfbd
md.jewel mia <md.jewelmia52@gmail.com>
2026-02-08 23:00:12 +0600

    add length of last word

--- goalgorithom/length_of_last_word.go
+++ goalgorithom/length_of_last_word.go
@@ -3,11 +3,22 @@ import "fmt"
 
 
 func lengthOfLastWord(s string) int {
-    
+	n := len(s)
+    result := 0
+	for i:=n-1; i >=0; i--{
+		if s[i] == ' '{
+			if result >0 {
+				break
+			}
+		}else{
+			result++
+		}
+	}
+	return  result
 }
 
 func main() {
-	s := "Hello World"
+	s := "Hello World "
 	result := lengthOfLastWord(s)
 	fmt.Println(result)
 }
