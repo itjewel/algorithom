3b6f9722e7f040ef100d1a3b87e064025a532d39
md.jewel mia <md.jewelmia52@gmail.com>
2026-02-08 23:08:07 +0600

    added longest common prefix

---
+++ goalgorithom/longest_common_prefix.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+
+func longestCommonPrefix(strs []string) string {
+    prefix := ""
+
+	return prefix
+}
+
+func main() {
+	commonString := []string{"flower","flow","flight"}
+	result := longestCommonPrefix(commonString)
+	fmt.Println(result)
+}
